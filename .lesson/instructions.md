b7df6bcdd112efcd3173ab36342586c6add01419
Maru Puran <144689560+mpur6520@users.noreply.github.com>
2024-03-17 19:56:16 +0000

    Initial commit

---
+++ .lesson/instructions.md
@@ -0,0 +1,19 @@
+# Task - Investigate
+
+Use comments to answer the investigate questions on the example code.
+
+## Help! My code doesn't work!
+
+Make sure that you check for the following things:
+
+- you have typed the variable name **exactly** the same (including caps), every time
+- There is a variable on the **left** of each assignment
+- You have used a single **=** for assignment
+- You have used the correct **operator**
+    - <pre>+ addition
+    - <pre>- subtraction
+    - <pre>* multiplication
+    - <pre>/ decimal division (gives numbers after the decimal point)
+    - <pre>// integer division (just gives the whole number part of the answer)
+- You have remembered the brackets after input:
+    - <pre> input()
---
+++ main.py
@@ -0,0 +1,32 @@
+#Maru Puran
+#September 28, 2023
+#analyze a code and answer questions to better understand arithmetic expressions + how they work with user inputs and variables
+
+# Starter code
+
+num1 = 25 #create varuable num1 and assign value of 25 to it
+num2 = 4 #create variable num2 and assign value of 4 to it
+print("Enter a number") #output text "enter a number" prompt
+num3 = int(input()) #create a variable to store user's response to prompt called num3, changes default string to an integer to store
+
+total1 = num1 * num2 #create variable to hold called total1, the product of num1 and num2 which is 100
+total2 = num3 + num2 #create variable called total2 to hold the sum of num3 (number the user inputted) and num2 (4) 
+total3 = total1 - total2 #create variable called total3 to hold the difference of total1 and total2
+
+# How many variables are used in the code?
+  # Answer: 6 variables are used in the code, num1, num2, num3, total1, total2, and total3.
+
+# Give the line number where an output statement is used.
+  # Answer: Line 5 has an output statement to ask the user to enter a number on screen.
+
+# What symbol is used for variable assignment?
+  # Answer: The equal sign (=) is used for variable assignments.
+
+# What value is stored in total1 at the end of the program?
+  # Answer: 100 is stored in total1 at the end of the program.
+
+# What value is stored in num3 at the end of the program?
+  # Answer: num3 is assigned the value the user inputs into the program once it runs.
+
+# Line 8 is changed to total1 = num1 // num2. How would it affect the program?
+  # Answer: Nothing changes on the output side. It changes the result of variable total3, assigning the value of 6 to total1 rather than 100.
